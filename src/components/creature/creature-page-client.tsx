90416ec9af0bbaa9689bb093950d424f39b266d0
Margouillat <erwanduchene@gmail.com>
2025-11-12 23:02:56 +0100

    fix: interval fetch monster

--- src/components/creature/creature-page-client.tsx
+++ src/components/creature/creature-page-client.tsx
@@ -130,7 +130,7 @@ export function CreaturePageClient ({ monster }: CreaturePageClientProps): React
 
     const interval = setInterval(() => {
       void fetchMonster()
-    }, 5000)
+    }, 2000)
 
     return () => clearInterval(interval)
   }, [monster, currentMonster])
